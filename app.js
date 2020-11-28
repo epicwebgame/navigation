1a5714ca8897379ef9af87ca69eaaefea3af2300
epicwebgame <52540418+epicwebgame@users.noreply.github.com>
2020-11-27 23:46:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ nav.classList.toggle('nav-active');
 });
 //Animate Links
   navLinks.forEach((link, index) =>{
-    link.style.animation = 'navLinkFade 0.5s ease forwards $(index / 7)s';
-    console.log(index / 7);
+    
+   if(link.style.animation){
+     link.style.animation = ' '
+   } 
+    else {
+      link.style.animation= 'navLinkFade 0.5s ease forwards $(index / 7 + 0.3)s
+    }
   });
 }
-
 
 navSlide();
